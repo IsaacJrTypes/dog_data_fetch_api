564caebe1ec17abd9e6751e144e5cea910b3d476
Isaac J <duckhunterjr@gmail.com>
2024-01-29 03:43:50 -0800

    add fact and group pages. Needs CSS

--- index.js
+++ index.js
@@ -3,18 +3,25 @@ import express from 'express';
 const app = express();
 const port = process.env.PORT || 3000;
 
-async function getDogAPIResponse(id = "") {
+async function getDogAPIResponse(param = "") {
     try {
         let response;
-        if (id == '') {
+        if (param === '') {
             response = await fetch('https://dogapi.dog/api/v2/breeds');
+
+        } else if (param === 'facts') {
+            response = await fetch('https://dogapi.dog/api/v2/facts');
+
+        } else if (param === 'groups') {
+            response = await fetch('https://dogapi.dog/api/v2/groups')
+
         } else {
-            response = await fetch('https://dogapi.dog/api/v2/breeds/' + id);
+            response = await fetch('https://dogapi.dog/api/v2/breeds/' + param);
         }
         console.log("Fetch status: " + response.status);
         return response.json();
     } catch (err) {
-        console.err('Fetch Err:', err);
+        console.log('Fetch Err:', err);
     }
 }
 
@@ -84,11 +91,70 @@ Task:
 [x] Add an event listener to each breed that fetches and displays detailed information when clicked.
 */
 app.get('/breed-list', async (req, res) => {
-    const apiResponse = await getDogAPIResponse();
-    res.type('text/html');
-    res.send(apiResponse.data.map((dog) => `<li><a href="/breeds/${dog.id}">${dog.attributes.name}</a></li>`).join(""));
+    try {
+        const apiResponse = await getDogAPIResponse();
+        res.type('text/html');
+        res.send(apiResponse.data.map((dog) => `<li><a href="/breeds/${dog.id}">${dog.attributes.name}</a></li>`).join(""));
+    } catch (err) {
+        console.log(err);
+        res.status(500).send("Server Error");
+    }
+
 
 });
+
+/*
+Part 5: Working with Dog Facts and Groups
+
+Goals:
+Fetch and display a list of random dog facts.
+Fetch and display information about different dog groups.
+
+Tasks:
+[x] Use the /facts endpoint to fetch dog facts.
+[] Display these facts in an interesting format on the webpage.
+[x] Use the /groups endpoint to fetch information about dog groups and display it.
+*/
+app.get('/facts', async (req, res) => {
+    try {
+        const apiResponse = await getDogAPIResponse('facts');
+        const factObj = apiResponse.data[0];
+        const html = `
+        <div>
+        <p>Fact</p>
+        <p>${factObj.attributes.body}</p>
+        </div>
+        `;
+
+        res.type('text/html');
+        res.send(html);
+        console.log(factObj);
+    } catch (err) {
+        console.log("Error: ", err);
+        res.status(500).send("Server Error");
+    }
+});
+
+app.get('/groups', async(req,res) => {
+    try {
+        const apiResponse = await getDogAPIResponse('groups')
+        console.log(apiResponse.data)
+        const html = `
+        <div>
+        <p>Dog Groups</p>
+        <ul>${apiResponse.data.map((group) => '<li>'+group.attributes.name+'</li>').join('')}
+        </ul>
+        </div>
+        `
+        res.type('text/html')
+        res.send(html);
+
+
+    } catch(err) {
+        console.log("Error: ", err);
+        res.status(500).send("Server Error");
+    }
+})
 
 app.use((req, res) => {
     res.type("text/plain");
